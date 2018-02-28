dfb6ffca879e54b686c6346e3b7d3b741881dfe3
Gr <morgan.graeme@gmail.com>
2018-02-27 17:15:18 -0800

    Initial commit

---
+++ app/src/main/java/com/example/testdatabase/testingenvironment/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.testdatabase.testingenvironment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("Friends");
+
+        databaseReference.setValue("Hello from some friends");
+    }
+
+
+
+}
